0adf9044cc527e6677911f8c74fba71a717d00fb
Muhammad Tahir Idrees <mtahiridrees@gmail.com>
2017-01-15 19:09:41 +0500

    routes file added

---
+++ module4-solution/src/routes.js
@@ -0,0 +1,47 @@
+(function () {
+'use strict';
+
+angular.module('MenuApp')
+.config(RoutesConfig);
+
+RoutesConfig.$inject = ['$stateProvider', '$urlRouterProvider'];
+function RoutesConfig($stateProvider, $urlRouterProvider) {
+
+  // Redirect to home page if no other URL matches
+  $urlRouterProvider.otherwise('/');
+
+  // *** Set up UI states ***
+  $stateProvider
+
+  // Home page
+  .state('home', {
+    url: '/',
+    templateUrl: 'src/menuapp/templates/home.template.html'
+  })
+
+  // categories list page
+  .state('categories', {
+    url: '/categories',
+    templateUrl: 'src/menuapp/templates/categories.template.html',
+    controller: 'CategoriesController as categories',
+    resolve: {
+      items: ['MenuDataService', function (MenuDataService) {
+        return MenuDataService.getAllCategories();
+      }]
+    }
+  })
+
+  .state('items', {
+    url: '/items/{categoryId}',
+    templateUrl: 'src/menuapp/templates/items.template.html',
+    controller: 'CategoryItemsController as itemsCtrl',
+    resolve: {
+      item: ['$stateParams', 'MenuDataService',
+            function ($stateParams, MenuDataService) {
+              return MenuDataService.getItemsForCategory($stateParams.categoryId);
+            }]
+    }
+  });
+}
+
+})();
